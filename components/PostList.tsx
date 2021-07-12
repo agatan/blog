2174a6c10bbabf9d54a8899982d4ae4436c8696f
agatan <agatan039@gmail.com>
2021-07-12 23:08:44 +0900

    Remove pagination

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -14,18 +14,12 @@ import { PostMeta } from "../lib/posts";
 
 type Props = {
   postMetas: ReadonlyArray<PostMeta>;
-  page?: number;
 };
 
 const POSTS_PER_PAGE = 10;
 
 export const PostList: React.FC<Props> = (props) => {
-  const { postMetas, page } = { page: 0, ...props };
-  const totalPages = (postMetas.length - 1) / POSTS_PER_PAGE + 1;
-  const slice = postMetas.slice(
-    POSTS_PER_PAGE * page,
-    POSTS_PER_PAGE * (page + 1)
-  );
+  const { postMetas } = props;
   return (
     <Container maxW="container.md">
       <VStack
@@ -35,23 +29,10 @@ export const PostList: React.FC<Props> = (props) => {
         paddingBottom="4"
         align="stretch"
       >
-        {slice.map((postMeta) => (
+        {postMetas.map((postMeta) => (
           <PostItem key={postMeta.slug} postMeta={postMeta} />
         ))}
       </VStack>
-      <Flex>
-        {page <= 0 ? null : (
-          <Link p="4" href={`/?page=${page - 1}`}>
-            <Text fontSize="lg">← Previous</Text>
-          </Link>
-        )}
-        <Spacer />
-        {page >= totalPages ? null : (
-          <Link p="4" href={`/?page=${page + 1}`}>
-            <Text fontSize="lg">Next →</Text>
-          </Link>
-        )}
-      </Flex>
     </Container>
   );
 };
