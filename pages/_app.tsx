4b446b6506aa7e3c0ddaa37ec53f222f617e3e44
agatan <agatan039@gmail.com>
2021-07-12 18:33:38 +0900

    Header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,26 @@
 import type { AppProps } from "next/app";
 import React from "react";
-import { ChakraProvider, Container } from "@chakra-ui/react";
+import { Box, ChakraProvider, Container, Divider, Heading, Text } from "@chakra-ui/react";
 
 import "prismjs/themes/prism-tomorrow.css";
 import Head from "next/head";
+import { Link } from "../components/Link";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Box backgroundColor="green.50">
+        <Container maxWidth="container.lg" backgroundColor="green.600">
+          <Heading padding="8" color="white" >
+            <Link href="/" color="white">
+              agatan blog
+            </Link>
+          </Heading>
+        </Container>
+        <Container maxWidth="container.lg" backgroundColor="white">
+          <Component {...pageProps} />
+        </Container>
+      </Box>
     </ChakraProvider>
   );
 };
