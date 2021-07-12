55890ce314ffb97c6d5204373067420a270e2284
agatan <agatan039@gmail.com>
2021-07-12 19:16:43 +0900

    Show avatar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,8 @@
 import type { AppProps } from "next/app";
 import React from "react";
-import { Box, ChakraProvider, Container, Divider, Heading, Text } from "@chakra-ui/react";
+import { Box, ChakraProvider, Container, Flex, Heading, Image, Spacer, Text, } from "@chakra-ui/react";
 
 import "prismjs/themes/prism-tomorrow.css";
-import Head from "next/head";
 import { Link } from "../components/Link";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
@@ -18,7 +17,20 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
           </Heading>
         </Container>
         <Container maxWidth="container.lg" backgroundColor="white">
-          <Component {...pageProps} />
+          <Flex paddingTop="4" paddingBottom="4">
+            <Component {...pageProps} />
+            <Container maxWidth="44">
+              <Flex direction="column" align="center" padding="4">
+                <Box position="relative" width="16" height="16">
+                  <Image src="/agatan.png" layout="fill" objectFit="cover" alt="agatan" />
+                </Box>
+                <Link href="https://twitter.com/@agatan_">
+                  <Text>@agatan</Text>
+                </Link>
+                <Spacer />
+              </Flex>
+            </Container>
+          </Flex>
         </Container>
       </Box>
     </ChakraProvider>
