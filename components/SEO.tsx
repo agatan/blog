228c99932950c194881a79584e99c128f205a8ed
agatan <agatan039@gmail.com>
2021-07-14 18:45:39 +0900

    Set og:image

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -3,16 +3,20 @@ import React from "react";
 
 const SITE_TITLE = "↗agatan blog↗";
 
-export const SEO: React.VFC<{ title: string; description?: string }> = (
-  props
-) => {
-  const { title, description } = props;
+export const SEO: React.VFC<{
+  title: string;
+  description?: string;
+  image?: string;
+}> = (props) => {
+  const { title, description, image } = props;
+  const imageUrl = image || "https://blog.agatan.dev/favicon.ico";
   return (
     <Head>
       <title>{`${title} | ${SITE_TITLE}`}</title>
       {description && <meta name="description" content={description} />}
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
+      <meta property="og:image" content={imageUrl} />
       {description && <meta property="og:description" content={description} />}
       <meta property="og:site_name" content={SITE_TITLE} />
       <meta property="twitter:card" content="summary" />
