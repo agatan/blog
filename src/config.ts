8f3aa9c79490cb7711e23ac367fbe867d53e0969
agatan <agatan039@gmail.com>
2023-09-14 09:29:50 +0900

    Update

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,16 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://blog.agatan.dev/",
+  author: "agatan",
+  desc: "agatan blog",
+  title: "agatan blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 10,
 };
 
-export const LOCALE = ["en-EN"]; // set to [] to use the environment default
+export const LOCALE = ["ja-JP", "en-EN"]; // set to [] to use the environment default
 
 export const LOGO_IMAGE = {
   enable: false,
