4c7100cb96f89163534c8ceb3802c510aa729ca4
agatan <agatan039@gmail.com>
2023-09-22 08:51:41 +0900

    Fix c++ tags

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,7 +1,8 @@
 import { slug as slugger } from "github-slugger";
 import type { BlogFrontmatter } from "@content/_schemas";
 
-export const slugifyStr = (str: string) => slugger(str);
+export const slugifyStr = (str: string) =>
+  str.toLowerCase() == "c++" ? "cpp" : slugger(str);
 
 const slugify = (post: BlogFrontmatter) =>
   post.postSlug ? slugger(post.postSlug) : slugger(post.title);
